669e7cd8999afcec67545a08b4599bc618e1b328
pratik <pratik.patil9113@gmail.com>
2025-05-09 20:55:30 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: [
-            "http://localhost:5173"
+            "https://government-scheme.vercel.app"
         ],
         methods: ["GET", "POST"],
         credentials:true,
@@ -42,7 +42,7 @@ const io = new Server(server, {
 // CORS configuration for frontend
 app.use(cors({
     origin: [
-        "http://localhost:5173"
+        "https://government-scheme.vercel.app"
     ], 
     credentials: true, // Allow credentials (cookies)
 }))
